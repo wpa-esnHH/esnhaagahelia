efc9c954b86228233242769fb76249b37f52409f
Victor <aue228lublupacanov@gmail.com>
2024-08-13 14:21:01 +0200

    config base path update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/esnhaagahelia',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
 })
