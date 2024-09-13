c3d83b251e4676ab6bce3aaa30df138eec055d31
wpa-esnHH <wpa@esnhaagahelia.eu>
2024-09-13 19:36:05 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -17,19 +17,20 @@ const blocksData = [
     },
     {
         id: 2,
-        title: "ESN Card",
-        description: "Description and details for Esn card",
-        price: "€15",
-        photo: esnCardPhoto,
-        linkToEvent: 'https://kide.app/memberships/b63177d3-6104-409b-8ad6-8c3d25f4a45d'
-    },
-    {
-        id: 3,
         title: "International Dinner",
         description: "Bring cultures together with traditional cuisine.",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsnKrttAUKwSJwPTz2DB8uFAPyk1qH1Q88CxbMi3AtKgJWp_5gfDMjsMMDz9kVmqc7G7w&usqp=CAU",
         date: "2024-02-28",
         linkToEvent: "https://activities.esn.org/activity/international-dinner-esn-helga-21483"
+    },
+    {
+        id: 3,
+        title: "ESN Card",
+        description: "Description and details for Esn card",
+        price: "€15",
+        photo: esnCardPhoto,
+        linkToEvent: 'https://kide.app/memberships/b63177d3-6104-409b-8ad6-8c3d25f4a45d'
+        
     },
     {
         id: 4,
